fcfa2c2b84239b4566eb29dd4b251c8e011e22de
SANJU <sanjubodra1420@gmail.com>
2024-02-08 13:39:52 +0530

    updated code

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def get_tweets():
 
 if __name__ == '__main__':
     scraper = Nitter()  # Initialize scraper in the __main__ function
-    app.run(host="0.0.0.0", port="5500", debug=True)
+    app.run(debug=True)
